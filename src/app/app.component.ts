5d774f118a1b27bc128636f200698c9698b4fea8
nydal91 <nydal91@hotmail.com>
2016-10-25 13:39:23 +0200

    modified:   angular-cli.json 	modified:   package.json 	modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.spec.ts 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	new file:   src/app/navbar/navbar.component.css 	new file:   src/app/navbar/navbar.component.html 	new file:   src/app/navbar/navbar.component.spec.ts 	new file:   src/app/navbar/navbar.component.ts 	new file:   src/assets/bygg.jpg 	new file:   src/assets/bygg2.jpg 	new file:   src/assets/bygg3.jpg 	new file:   src/assets/grav.jpg 	modified:   src/index.html 	modified:   src/styles.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ng2BootstrapConfig, Ng2BootstrapTheme } from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+ title = 'Vikafisk AS';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { Ng2BootstrapConfig, Ng2BootstrapTheme } from 'ng2-bootstrap/ng2-bootstrap';
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { CollapseModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { DropdownModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AlertModule,
+    CollapseModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
